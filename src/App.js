cddc462fcee3d4717015e6f7479325718cc1f5bf
mablezhang2003 <154645068+mablezhang2003@users.noreply.github.com>
2025-06-04 12:04:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Education from "./components/Education";
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Welcome from './pages/Welcome';
+import About from './pages/About';
+import Projects from './components/Projects'; 
+import Contact from './components/Contact';  
+import Education from './components/Education'; 
 
 function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <main>
-                <About/>
-                <Education/>
-                <Projects/>
-                <Contact/>
-            </main>
-            <Footer/>
-        </div>
-    );
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
