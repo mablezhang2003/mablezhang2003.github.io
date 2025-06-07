71f7bdaadd2de808899d04ceb90519d198d6646b
mablezhang2003 <154645068+mablezhang2003@users.noreply.github.com>
2025-06-07 13:31:53 +0800

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import '../App.css';
+import trashCleanupImg from '../assets/trashcleanup.png';
+import worldGenImg from '../assets/world-generation.png';
+import cookCountyImg from '../assets/cook-county.png';
 
 function Projects() {
   return (
@@ -9,19 +12,19 @@ function Projects() {
       <div className="projects-grid">
         <ProjectCard
           title="Trash Cleanup Optimal Routing "
-          image="/assets/trashcleanup.png"
+          image={trashCleanupImg}
           description="Developed a routing algorithm for trash collection in New York City using Vehicle Routing Problem (VRP) and a Geodesic Distance matrix in Python, used heat maps and clustering to identify high-density areas and prioritize cleanup efforts"
         />
 
         <ProjectCard
           title="World Generation Game"
-          image="/assets/world-generation.png"
+          image={worldGenImg}
           description='Developed a routing algorithm for trash collection in New York City using Vehicle Routing Problem (VRP) and a Geodesic Distance matrix in Python, used heat maps and clustering to identify high-density areas and prioritize cleanup efforts'
         />
 
         <ProjectCard
           title="Cook County Housing Gov Data Set"
-          image="/assets/cook-county.png"
+          image={cookCountyImg}
           description="Built a data pipeline to remove outliers and transform data to feed into the model and conducted feature engineering
 to train a linear regression model to determine the predictive power of 60+ features on housing price"
         >
