e9148f9b9ee596e6e70c378438963ca819cedc92
mablezhang2003 <154645068+mablezhang2003@users.noreply.github.com>
2025-06-09 21:06:38 +0800

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function ProjectCard({ title, image, description, link, children }) {
+function ProjectCard({ title, image, description, downloadLink, children }) {
   return (
     <div className="project-card">
       {image && <img src={image} alt={title} className="project-image" />}
       <h3>{title}</h3>
       <p>{description}</p>
       {children}
-      {link && (
-        <a className="project-link" href={link} target="_blank" rel="noreferrer">
-          Visit Project
+      {downloadLink && (
+        <a className="project-link" href={downloadLink} download>
+          Download Code
         </a>
       )}
     </div>
