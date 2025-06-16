0309b961afd6b84e47d69490bb7f5a5a51c2d1e2
mablezhang2003 <154645068+mablezhang2003@users.noreply.github.com>
2025-06-17 03:21:27 +0800

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,18 +33,21 @@ function Projects() {
           title="Feedforward and Convolutional Neural Networks"
           image={neuralNetworkImg}
           description="Implemented a two-layer neural network and CNN from scratch using NumPy. Trained models on Iris and MNIST datasets. Explored learning rates, network sizes, and transfer learning with CIFAR-10 in PyTorch."
+          githubLink="https://github.com/mablezhang2003/fc-cnn"
         />
 
         <ProjectCard
           title="Random Forest on Titanic Survivorship"
           image={decisionRandomImg}
           description="Built decision tree and random forest models for classification tasks on Titanic dataset, with custom depth control, missing value handling, and validation accuracy exceeding 80%."
+          githubLink="https://github.com/mablezhang2003/random-forest"
         />
 
         <ProjectCard
           title="Cook County Housing Gov Data Set"
           image={cookCountyImg}
           description="Built a data pipeline to remove outliers and transform data, conducted feature engineering, and trained a linear regression model to predict housing prices from 60+ features."
+          githubLink="https://github.com/mablezhang2003/cook-county"
 
         />
 
@@ -52,6 +55,7 @@ function Projects() {
           title="Ants vs. Bees Game"
           image={antsBeesImg}
           description="Built a Python tower defense game with ant units that collect food, throw projectiles, absorb damage, and apply status effects to invading bees, all using class inheritance and a real-time GUI."
+          githubLink="https://github.com/mablezhang2003/ants-bees"
 
         />
       </div>
